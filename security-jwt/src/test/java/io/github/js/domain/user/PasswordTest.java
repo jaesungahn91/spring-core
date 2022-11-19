7b44be666ddbbed63ad1c3e0fdf1c7be92ca75e2
jamieAhn <jaesungahn91@gmail.com>
2022-11-19 15:21:37 +0900

    temp

---
+++ security-jwt/src/test/java/io/github/js/domain/user/PasswordTest.java
@@ -0,0 +1,16 @@
+package io.github.js.domain.user;
+
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+@ExtendWith(MockitoExtension.class)
+class PasswordTest {
+
+    @Mock
+    PasswordEncoder passwordEncoder;
+
+
+
+}
