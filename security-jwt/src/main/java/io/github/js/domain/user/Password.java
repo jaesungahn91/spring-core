dc14e4acfe6dd94673510248eaf017a87d45eb63
jamieAhn <jaesungahn91@gmail.com>
2022-11-19 14:00:59 +0900

    feat: user 도메인 추가

---
+++ security-jwt/src/main/java/io/github/js/domain/user/Password.java
@@ -0,0 +1,29 @@
+package io.github.js.domain.user;
+
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+import javax.persistence.Column;
+import javax.persistence.Embeddable;
+
+@Embeddable
+public class Password {
+
+    @Column(name = "password", nullable = false)
+    private String encodedPassword;
+
+    public static Password of(String rawPassword, PasswordEncoder passwordEncoder) {
+        return new Password(passwordEncoder.encode(rawPassword));
+    }
+
+    private Password(String encodedPassword) {
+        this.encodedPassword = encodedPassword;
+    }
+
+    protected Password() {
+    }
+
+    public boolean matchesPassword(String rawPassword, PasswordEncoder passwordEncoder) {
+        return passwordEncoder.matches(rawPassword, encodedPassword);
+    }
+
+}
---
+++ security-jwt/src/main/java/io/github/js/domain/user/User.java
@@ -0,0 +1,32 @@
+package io.github.js.domain.user;
+
+import javax.persistence.*;
+
+import static javax.persistence.GenerationType.*;
+
+@Entity
+@Table(name = "users")
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = IDENTITY)
+    private Long id;
+
+    private String email;
+
+    @Embedded
+    private Password password;
+
+    public static User of(String email, Password password) {
+        return new User(email, password);
+    }
+
+    private User(String email, Password password) {
+        this.email = email;
+        this.password = password;
+    }
+
+    protected User() {
+    }
+
+}
