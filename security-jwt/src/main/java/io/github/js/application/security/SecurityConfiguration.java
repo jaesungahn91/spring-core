84513d3e0dae451ca3d23502c786f7432eff838c
jaesungahn91 <jaesungahn91@gmail.com>
2022-11-22 15:38:02 +0900

    feat: exceptionHandling()

--- security-jwt/src/main/java/io/github/js/application/security/SecurityConfiguration.java
+++ security-jwt/src/main/java/io/github/js/application/security/SecurityConfiguration.java
@@ -1,5 +1,6 @@
 package io.github.js.application.security;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import io.github.js.domain.jwt.JWTDeserializer;
 import lombok.RequiredArgsConstructor;
 import org.springframework.boot.context.properties.ConfigurationProperties;
@@ -7,6 +8,7 @@ import org.springframework.boot.context.properties.ConstructorBinding;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.MediaType;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
@@ -15,9 +17,13 @@ import org.springframework.security.web.authentication.UsernamePasswordAuthentic
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
 import java.util.List;
+import java.util.Map;
 
 import static org.springframework.http.HttpMethod.POST;
+import static org.springframework.http.MediaType.APPLICATION_JSON_VALUE;
 import static org.springframework.security.config.http.SessionCreationPolicy.STATELESS;
 
 @EnableConfigurationProperties(SecurityConfigurationProperties.class)
@@ -26,6 +32,7 @@ import static org.springframework.security.config.http.SessionCreationPolicy.STA
 public class SecurityConfiguration implements WebMvcConfigurer {
 
     private final SecurityConfigurationProperties properties;
+    private final ObjectMapper objectMapper;
 
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
@@ -39,6 +46,14 @@ public class SecurityConfiguration implements WebMvcConfigurer {
         http.authorizeRequests()
                 .antMatchers(POST, "/users", "/users/login").permitAll()
                 .anyRequest().authenticated();
+        http.exceptionHandling()
+                .accessDeniedHandler((request, response, accessDeniedException) -> {
+                    sendError(response, "권한이 유효하지 않습니다.", 403);
+                })
+                .authenticationEntryPoint((request, response, accessDeniedException) -> {
+                    sendError(response, "토큰이 유효하지 않습니다.", 401);
+                });
+
         return http.build();
     }
 
@@ -60,6 +75,14 @@ public class SecurityConfiguration implements WebMvcConfigurer {
                 .allowedHeaders("*")
                 .allowCredentials(true);
     }
+
+    private void sendError(HttpServletResponse response, String message, int status) throws IOException {
+        response.setContentType(APPLICATION_JSON_VALUE);
+        response.setCharacterEncoding("UTF-8");
+        response.setStatus(status);
+        response.getWriter().write(objectMapper.writeValueAsString(Map.of("message", message)));
+    }
+
 }
 
 @ConstructorBinding
