a2024c8e120afcfe603037c71b394ea450cdfe11
jaesungahn91 <jaesungahn91@gmail.com>
2022-11-30 17:08:19 +0900

    feat: order test

--- spring-rest-docs/src/main/java/io/github/js/application/order/OrderModel.java
+++ spring-rest-docs/src/main/java/io/github/js/application/order/OrderModel.java
@@ -1,4 +1,22 @@
 package io.github.js.application.order;
 
+
+import lombok.Getter;
+
+@Getter
 public class OrderModel {
+
+    private final String menuName;
+
+    private final Integer quantity;
+
+    public static OrderModel of(String menuName, Integer quantity) {
+        return new OrderModel(menuName, quantity);
+    }
+
+    private OrderModel(String menuName, Integer quantity) {
+        this.menuName = menuName;
+        this.quantity = quantity;
+    }
+
 }
--- spring-rest-docs/src/main/java/io/github/js/application/order/OrderRestController.java
+++ spring-rest-docs/src/main/java/io/github/js/application/order/OrderRestController.java
@@ -1,14 +1,19 @@
 package io.github.js.application.order;
 
+import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.validation.Valid;
+
+@RequiredArgsConstructor
 @RestController
 public class OrderRestController {
 
     @PostMapping("/orders")
-    public OrderModel postOrder(OrderPostRequestDTO dto) {
-        return null;
+    public OrderModel postOrder(@Valid @RequestBody OrderPostRequestDTO dto) {
+        return OrderModel.of(dto.getMenuName(), dto.getQuantity());
     }
 
 }
