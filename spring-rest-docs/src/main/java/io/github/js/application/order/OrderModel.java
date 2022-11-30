6f8cdd8ca9c04605078a69512f98a829a0158641
jaesungahn91 <jaesungahn91@gmail.com>
2022-11-30 17:50:50 +0900

    feat: docs 관련 설정

--- spring-rest-docs/src/main/java/io/github/js/application/order/OrderModel.java
+++ spring-rest-docs/src/main/java/io/github/js/application/order/OrderModel.java
@@ -1,8 +1,15 @@
 package io.github.js.application.order;
 
 
+import com.fasterxml.jackson.annotation.JsonTypeInfo;
+import com.fasterxml.jackson.annotation.JsonTypeName;
 import lombok.Getter;
 
+import static com.fasterxml.jackson.annotation.JsonTypeInfo.As.WRAPPER_OBJECT;
+import static com.fasterxml.jackson.annotation.JsonTypeInfo.Id.NAME;
+
+@JsonTypeName("order")
+@JsonTypeInfo(include = WRAPPER_OBJECT, use = NAME)
 @Getter
 public class OrderModel {
 
--- spring-rest-docs/src/test/java/io/github/js/application/order/OrderRestControllerTest.java
+++ spring-rest-docs/src/test/java/io/github/js/application/order/OrderRestControllerTest.java
@@ -9,6 +9,9 @@ import org.springframework.http.HttpStatus;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.springframework.http.MediaType.APPLICATION_JSON_VALUE;
+import static org.springframework.restdocs.payload.JsonFieldType.NUMBER;
+import static org.springframework.restdocs.payload.JsonFieldType.STRING;
+import static org.springframework.restdocs.payload.PayloadDocumentation.*;
 import static org.springframework.restdocs.restassured3.RestAssuredRestDocumentation.document;
 
 class OrderRestControllerTest extends AcceptanceTest {
@@ -31,8 +34,18 @@ class OrderRestControllerTest extends AcceptanceTest {
         OrderPostRequestDTO dto = new OrderPostRequestDTO(menuName, quantity);
 
         return RestAssured
-                .given().spec(this.spec)
-                .filter(document("post-order"))
+                .given()
+
+                .spec(this.spec)
+                .accept(APPLICATION_JSON_VALUE)
+                .filter(document("post-order",
+                        requestFields(
+                                fieldWithPath("order.menuName").type(STRING).description("order menu name"),
+                                fieldWithPath("order.quantity").type(NUMBER).description("order quantity")),
+                        responseFields(
+                                fieldWithPath("order.menuName").type(STRING).description("order menu name"),
+                                fieldWithPath("order.quantity").type(NUMBER).description("order quantity"))
+                ))
                 .log().all()
                 .body(dto)
                 .contentType(APPLICATION_JSON_VALUE)
