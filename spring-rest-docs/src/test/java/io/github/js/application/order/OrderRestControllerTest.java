ce8be7c15d8d633e4af07d50202472835dfe510f
jaesungahn91 <jaesungahn91@gmail.com>
2022-12-04 17:10:38 +0900

    refactor: snippet 메소드화

--- spring-rest-docs/src/test/java/io/github/js/application/order/OrderRestControllerTest.java
+++ spring-rest-docs/src/test/java/io/github/js/application/order/OrderRestControllerTest.java
@@ -5,8 +5,10 @@ import io.restassured.RestAssured;
 import io.restassured.response.ExtractableResponse;
 import io.restassured.response.Response;
 import org.junit.jupiter.api.Test;
-import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
+import org.springframework.restdocs.headers.RequestHeadersSnippet;
+import org.springframework.restdocs.payload.RequestFieldsSnippet;
+import org.springframework.restdocs.payload.ResponseFieldsSnippet;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.springframework.http.HttpHeaders.AUTHORIZATION;
@@ -36,17 +38,9 @@ class OrderRestControllerTest extends AcceptanceTest {
 
     private ExtractableResponse<Response> 주문_생성_요청(String menuName, Integer quantity) {
         final var restDocumentationFilter = document("post-order",
-                requestHeaders(
-                        headerWithName(AUTHORIZATION).description("Bearer token")
-                ),
-                requestFields(
-                        fieldWithPath("order.menuName").type(STRING).description("order menu name"),
-                        fieldWithPath("order.quantity").type(NUMBER).description("order quantity")
-                ),
-                responseFields(
-                        fieldWithPath("order.menuName").type(STRING).description("order menu name"),
-                        fieldWithPath("order.quantity").type(NUMBER).description("order quantity")
-                )
+                getRequestHeadersSnippet(),
+                getOrderRequestFieldsSnippet(),
+                getOrderResponseFieldsSnippet()
         );
 
         OrderPostRequestDTO dto = new OrderPostRequestDTO(menuName, quantity);
@@ -60,6 +54,26 @@ class OrderRestControllerTest extends AcceptanceTest {
                 .when().post("/orders")
                 .then().log().all()
                 .extract();
+    }
+
+    private RequestHeadersSnippet getRequestHeadersSnippet() {
+        return requestHeaders(
+                headerWithName(AUTHORIZATION).description("Bearer token")
+        );
+    }
+
+    private RequestFieldsSnippet getOrderRequestFieldsSnippet() {
+        return requestFields(
+                fieldWithPath("order.menuName").type(STRING).description("order menu name"),
+                fieldWithPath("order.quantity").type(NUMBER).description("order quantity").optional()
+        );
+    }
+
+    private ResponseFieldsSnippet getOrderResponseFieldsSnippet() {
+        return responseFields(
+                fieldWithPath("order.menuName").type(STRING).description("order menu name"),
+                fieldWithPath("order.quantity").type(NUMBER).description("order quantity")
+        );
     }
 
 }
