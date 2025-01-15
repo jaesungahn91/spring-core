506e8203279b4f05449c6e044091aa2022f16299
jaesungahn91 <jaesungahn91@gmail.com>
2025-01-15 23:11:12 +0900

    feat: security X-Frame-Options 설정 추가

--- spring-security-jwt/src/main/java/io/github/js/application/security/SecurityConfiguration.java
+++ spring-security-jwt/src/main/java/io/github/js/application/security/SecurityConfiguration.java
@@ -8,7 +8,6 @@ import org.springframework.boot.context.properties.ConstructorBinding;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.http.MediaType;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
@@ -36,6 +35,7 @@ public class SecurityConfiguration implements WebMvcConfigurer {
 
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http.headers().frameOptions().sameOrigin();
         http.csrf().disable();
         http.cors();
         http.formLogin().disable();
