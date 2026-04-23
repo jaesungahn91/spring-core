7596b86a8c66a437a7a045899036aacd8d28cc57
jaesung-ahn <jaesungahn91@gmail.com>
2026-04-23 17:22:21 +0900

    chore: QueryDSL 5.1 기반 구축 (의존성 + JPAQueryFactory 빈) Closes #98

--- spring-data-jpa/src/main/java/io/github/js/infrastructure/config/JpaConfig.java
+++ spring-data-jpa/src/main/java/io/github/js/infrastructure/config/JpaConfig.java
@@ -1,9 +1,17 @@
 package io.github.js.infrastructure.config;
 
+import com.querydsl.jpa.impl.JPAQueryFactory;
+import jakarta.persistence.EntityManager;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @Configuration
 @EnableJpaAuditing(auditorAwareRef = "securityAuditorAware")
 public class JpaConfig {
+
+    @Bean
+    public JPAQueryFactory jpaQueryFactory(EntityManager em) {
+        return new JPAQueryFactory(em);
+    }
 }
